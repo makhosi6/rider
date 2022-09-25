2d043af4ce2e8e5784ec71b19b92ade680a94433
makhosi6 <ndondo330@gmail.com>
2022-09-25 23:00:46 +0200

    iNiTial CoMmIt

---
+++ lib/components/flutter_google_maps_bottom_sheet.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+class GoogleMapsClonePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          CustomGoogleMap(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.55,
+              child: DraggableScrollableSheet(
+                initialChildSize: 0.37,
+                minChildSize: 0.37,
+                snap: true,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return SingleChildScrollView(
+                    controller: scrollController,
+                    child: const CustomScrollViewContent(),
+                  );
+                },
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 45.0,
+            left: 10.0,
+            // alignment: Alignment.topLeft,
+            child: RoundBtn(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// Google Map in the background
+class CustomGoogleMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[50],
+      child: const Center(child: Text("Google Map here")),
+    );
+  }
+}
+
+/// Content of the DraggableBottomSheet's child SingleChildScrollView
+class CustomScrollViewContent extends StatelessWidget {
+  const CustomScrollViewContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      margin: const EdgeInsets.all(0),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: CustomInnerContent(),
+      ),
+    );
+  }
+}
+
+class CustomInnerContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          CustomDraggingHandle(),
+          Flexible(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 6,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: ListView.builder(
+                  itemCount: 23,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CustomFeaturedItem();
+                  }),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    // ignore: prefer_const_constructors
+                    icon: Icon(
+                      color: Colors.blue,
+                      Icons.tune,
+                      size: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDraggingHandle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 4,
+      width: 40,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
+  }
+}
+
+class RoundBtn extends StatelessWidget {
+  const RoundBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        primary: const Color.fromARGB(255, 155, 207, 255),
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(12.0),
+      ),
+      child: const Icon(Icons.menu),
+    );
+  }
+}
+
+class CustomRecentPhotoLarge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: CustomFeaturedItem(),
+    );
+  }
+}
+
+class CustomFeaturedItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 15.0,
+        right: 20.0,
+      ),
+      child: Container(
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.grey[500],
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
+// https://github.com/surfstudio/flutter-bottom-sheet/blob/main/lib/src/flexible_bottom_sheet.dart
